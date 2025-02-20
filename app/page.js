80e7973d59e0b049b147893790b1aead10780496
mxzyy <hilmyp53@gmail.com>
2025-02-20 18:15:49 +0700

    FIx data fetch

--- app/page.js
+++ app/page.js
@@ -1,36 +1,35 @@
-"use client"
-import { useState, useEffect } from "react";
 
-// export async function getServerSideProps() {
-//   // Fetch data from external API
-//   const res = await fetch('localhost:5000/data1')
-//   const repo = await res.json()
-//   // Pass data to the page via props
-//   return { props: { repo } }
-// }
 
-export default function Main() {
-  const [models, setModels] = useState([]);
+async function getData() {
   const API_URL = process.env.NEXT_PUBLIC_API_URL;
-  useEffect(() => {
+  const res = await fetch(API_URL, {mode: "cors"});
+  if (!res.ok) console.log("error");
+  return (await res.json());
+}
+
+
+export default async function Main() {
+  const models = await getData();
+
+  // useEffect(() => {
     
-    const fetchData = async () => {
-      console.log(API_URL);
-      try {
-        const response = await fetch(API_URL, {mode: "cors"});
-        console.log("Raw response:", response); // Cek response mentah
+  //   const fetchData = async () => {
+  //     console.log(API_URL);
+  //     try {
+  //       const response = await fetch(API_URL, {mode: "cors"});
+  //       console.log("Raw response:", response); // Cek response mentah
 
-        const data = await response.json();
-        setModels(data);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-        console.error("Error type:", error.name);
-        console.error("Error message:", error.message);
-      }
-    };
+  //       const data = await response.json();
+  //       setModels(data);
+  //     } catch (error) {
+  //       console.error("Error fetching data:", error);
+  //       console.error("Error type:", error.name);
+  //       console.error("Error message:", error.message);
+  //     }
+  //   };
 
-    fetchData();
-  }, [API_URL]);
+  //   fetchData();
+  // }, [API_URL]);
 
   return (
     <div className="mx-8 mt-3 w-[92rem] h-[50rem] rounded-md ">
